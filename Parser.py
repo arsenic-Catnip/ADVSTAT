2cd4878cce9fae6933fb6ff561d42e4678c29fd4
angpulangpluma <mariennelopez30@gmail.com>
2016-11-28 12:12:23 +0800

    extracts all words from each text file

--- Parser.py
+++ Parser.py
@@ -6,6 +6,7 @@
 
 import argparse
 import os
+import re
 
 # constructing argument parse
 # when running this file type "python filechecker.py -s file1 -c file2" in cmd 
@@ -16,10 +17,11 @@ args = vars(ap.parse_args())
 
 
 word_results = open('wordresults.txt', 'a')
-word_results.write('wordlist of documents found in' + args['starting'])
-results = open('filelist.txt', 'a')
-results.write('-------------\n')
-results.write('filelist of ' + args['starting'] + '\n')
+word_results.write('-------------\n')
+word_results.write('wordlist of documents found in' + args['starting'] + '\n')
+# results = open('filelist.txt', 'a')
+# results.write('-------------\n')
+# results.write('filelist of ' + args['starting'] + '\n')
 files = 0
 for dirname, dirnames, filenames in os.walk(args['starting']):
 
@@ -27,12 +29,20 @@ for dirname, dirnames, filenames in os.walk(args['starting']):
 		#print(os.path.join(dirname, filename))
 			# results.write(os.path.join(dirname, subdirname) + '\n')
 		if filename.endswith('.txt'):
-			results.write(os.sep.join([dirname, filename])+ '\n')
+			# results.write(os.sep.join([dirname, filename])+ '\n')
 			fpath = os.path.join(dirname, filename)
+			word_results.write('-------------\n')
+			word_results.write("wordlist of " + fpath + ": \n")
 			f = open(fpath)
-			content = f.read()
+			for line in f:
+				# print(line)
+				list = line.split()
+				for word in list:
+					word_results.write(str(word) + "\n")
+			# content = f.read()
+			# word_results.write(content + "\n")
 			files = files+1
                                         
 print('Successfully found '+ str(files) + ' files in '+ args['starting'])
-results.write('# of files found'+ str(files))
-print('Words found in the documents: ' + '' + str(content))
+# results.write('# of files found'+ str(files))
+# print('Words found in the documents: ' + '' + str(content))
